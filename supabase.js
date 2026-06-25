80c283472fd92514b75d4d93ae88974f0c39fc55
wbaobao999-creator <wbaobao999@gmail.com>
2026-06-25 19:49:30 +0900

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,1 @@
-import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://duaaijqbngltmlgbzrvt.supabase.co";
-
-const supabaseKey = "你的真实 Publishable key";
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
